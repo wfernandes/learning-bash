0e52dcd96f1bc30e83b42f26950508fb7bb30870
Warren Fernandes <wfernandes@symplified.com>
2013-07-19 15:08:41 -0600

    Opens Chrome with bunch of start sites

--- GoodMorning/goodmorning.sh
+++ GoodMorning/goodmorning.sh
@@ -8,6 +8,18 @@ echoG 'Good Morning Warren...'
 REPO="/Users/wfernandes/Dev/Symplified/IntelliJGit/symplified-main/"
 echo "Update the repository"
 cd "$REPO"
-git pull
+#git pull
 
+# Open up browsers with list of my morning sites
+BBC="http://bbc.co.uk/news"
+TC="http://techcrunch.com"
+GMAIL="http://gmail.com"
+TED="http://ted.com"
 
+SITES=( $BBC $TC $GMAIL $TED ) 
+
+echo "Opening browser with sites..."
+for site in "${SITES[@]}"
+do
+  open -a "Google Chrome" $site
+done
