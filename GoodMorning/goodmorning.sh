3fb087f61fec198322531a150f4fa6a649eb460e
Warren Fernandes <wfernandes@symplified.com>
2013-07-22 19:56:59 -0600

    Fixed the issue with opening the IntelliJ app

--- GoodMorning/goodmorning.sh
+++ GoodMorning/goodmorning.sh
@@ -7,8 +7,11 @@ echoG 'Good Morning Warren...'
 # Update the repositories...
 REPO="/Users/wfernandes/Dev/Symplified/IntelliJGit/symplified-main/"
 echo "Update the repository"
-cd "$REPO"
-#git pull
+cd "$REPO"                                      # I want the user to end up in the repo directory for this terminal
+git pull
+if [ "$?" -ne "0"]; then
+  echoR "Git pull failed"
+fi
 
 # Open up browsers with list of my morning sites
 BBC="http://bbc.co.uk/news"
@@ -22,17 +25,23 @@ echo "Opening browser with sites..."
 for site in "${SITES[@]}"
 do
   open -a "Google Chrome" $site
+  if ["$?" -ne "0" ]; then
+    echoR "Could not open site $site"
+  fi
 done
 
 # Open up required applications
 INTELLIJ="/Applications/IntelliJ IDEA 12.app/Contents/MacOS/idea"
 GITX="/Applications/GitX.app/Contents/MacOS/GitX"
 
-APPS=( $INTELLIJ $GITX )
+APPS=( "$INTELLIJ" $GITX )                    # Using the quotes to resolve the whitespace in the INTELLIJ path
 
 echo "Opening applications..."
 for app in "${APPS[@]}"
 do
-  open $app
+  open "$app"                                   
+  if [ "$?" -ne "0" ]; then
+    echoR "Could not open application $a"
+  fi
 done
 
